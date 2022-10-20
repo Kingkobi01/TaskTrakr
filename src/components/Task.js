c3042923be99fe77e9faaf2a4ceb211a62d00d7f
Kwabena Boakye <kwaboakye93@gmail.com>
2022-10-20 16:41:39 +0000

    Additional tweaks

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ import React from "react";
 import { FaTimes } from "react-icons/fa";
 
 
-export default function Task(props) {
+export default function Task({ id, completed, job, date, time, toggle, onDelete }) {
     const taskStyle = {
         display: "flex",
         marginBottom: "1em",
@@ -12,14 +12,17 @@ export default function Task(props) {
         background: "#d0a3a3",
         paddingTop: ".25em",
         alignItems: "center",
-        paddingLeft: props.completed ? "0" : ".5em",
+        paddingLeft: completed ? "0" : ".5em",
         justifyContent: "space-between"
 
     }
 
 
-    return <div style={taskStyle} className="task" >
-        {props.completed && <div style={{
+
+    return <div style={taskStyle} className="task"
+        // onClick={() => toggle(id)}
+    >
+        {completed && <div style={{
             height: "4em",
             width: "1.5%",
             background: "green",
@@ -27,17 +30,15 @@ export default function Task(props) {
 
         }} className="completed"></div>}
         <div className="details">
-            <h3
-                onClick={() => props.toggle(props.id)}>
-                {props.job}</h3>
-            <p>{`${props.date} at ${props.time}`}</p>
+            <h3>{job}</h3>
+            <p>{`${date} at ${time}`}</p>
         </div>
         <div className="delete"
-            onClick={() => props.deleteTask(props.id)}>
 
-            <FaTimes style={{
-                color: "red"
-            }} />
+        >
+
+            <FaTimes color="red" cursor="pointer" onClick={() => onDelete(id)} />
         </div>
-    </div>
+
+    </div >
 }
