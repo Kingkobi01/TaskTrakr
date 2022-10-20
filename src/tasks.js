24ddadc5066797e743a31459ccdfc05b219e8969
Kwabena Boakye <kwaboakye93@gmail.com>
2022-10-20 16:49:06 +0000

    tasks

--- src/tasks.js
+++ src/tasks.js
@@ -1,30 +1,30 @@
 import Task from "./components/Task"
 import { useState } from "react"
 
+const tasks = [
+    {
+        id: 0,
+        job: "Doctor's Appointment",
+        date: "Feb 5th",
+        time: "2:30pm",
+        completed: true
+    },
+    {
+        id: 1,
+        job: "Meeting at School",
+        date: "Feb 6th",
+        time: "1:30pm",
+        completed: false
+    },
+    {
+        id: 2,
+        job: "Dinner with Mom",
+        date: "Feb 8th",
+        time: "7:00pm",
+        completed: true
+    },
+]
 export default function Tasks() {
-    const tasks = [
-        {
-            id: 0,
-            job: "Doctor's Appointment",
-            date: "Feb 5th",
-            time: "2:30pm",
-            completed: true
-        },
-        {
-            id: 1,
-            job: "Meeting at School",
-            date: "Feb 6th",
-            time: "1:30pm",
-            completed: false
-        },
-        {
-            id: 2,
-            job: "Dinner with Mom",
-            date: "Feb 8th",
-            time: "7:00pm",
-            completed: true
-        },
-    ]
 
     const [taskData, setTaskData] = useState(tasks)
 
